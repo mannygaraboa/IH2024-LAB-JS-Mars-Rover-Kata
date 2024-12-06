66ed779d7ec9f39696fd2d23756f41455e783867
Manuel Garaboa <mannygaraboa@gmail.com>
2024-12-05 23:36:22 -0500

    LAB: JS - Mar Rover Kata: Progress #1

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 // Rover object goes here:
+let rover = {
+    direction: "N",
+    x: 0,
+    y: 0
+}
 
 // =================
 function turnLeft(rover) {
-    
+    switch (rover.direction) {
+        case "N":
+            rover.direction = "W";
+            break;
+        case "W":
+            rover.direction = "S";
+            break;
+        case "S":
+            rover.direction = "E";
+            break;
+        case "E":
+            rover.direction = "N";
+            break;
+    }
+    console.log(`turnLeft was called, rover is facing ${rover.direction}`);
 }
 
 function turnRight(rover) {
-
+    switch (rover.direction) {
+        case "N":
+            rover.direction = "E";
+            break;
+        case "E":
+            rover.direction = "S";
+            break;
+        case "S":
+            rover.direction = "W";
+            break;
+        case "W":
+            rover.direction = "N";
+            break;
+    }
+    console.log(`turnRight was called, rover is facing ${rover.direction}`);
 }
 
 function moveForward(rover) {
 
 }
+
+turnLeft(rover);
+turnLeft(rover);
+turnRight(rover);
